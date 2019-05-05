df00ece48db437fa40550ca61d71188aa1a95ce7
Charl Jansen van Vuuren <charljvv@gmail.com>
2019-05-05 19:34:21 +0100

    Env variables for allEndpoint and header for cron job script

--- lambdas/allEndpoint.js
+++ lambdas/allEndpoint.js
@@ -3,10 +3,17 @@ const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.list = (event, context, callback) => {
+  var limit;
+  if (process.env.LIMIT) {
+    limit = process.env.LIMIT;
+  } else {
+    limit = 20;
+  }
+
   var params = {
     TableName: process.env.DBNAME,
     ProjectionExpression: "messageId, messageAttributes",
-    Limit: 10
+    Limit: limit
   };
 
   const onScan = (err, data) => {
--- sensors_to_cloud.py
+++ sensors_to_cloud.py
@@ -1,4 +1,6 @@
 
+#!/usr/bin/python
+
 import boto3 
 import datetime
 import fishtank
